7a049a0ecbf2060989ce41cd4b6be7af9786b5cc
Yogita Singh <115174566+YogitaSingh9@users.noreply.github.com>
2023-07-27 10:40:38 +0530

    Add files via upload

---
+++ Introduction/Find_the_percentage.py
@@ -0,0 +1,23 @@
+'''The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. Print the average of the marks array for the student name provided, showing 2 places after the decimal.'''
+
+n = int(input())
+
+student_marks = {}
+
+for _ in range(n):
+
+    name, *line = input().split()
+
+    scores = list(map(float, line))
+
+    student_marks[name] = scores
+
+query_name = input()
+
+l1 = list(student_marks[query_name]) 
+
+addition = sum(l1)
+
+result = addition/len(l1)
+
+print('%.2f'% result)
