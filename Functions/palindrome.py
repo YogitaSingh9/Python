f0e22d913e23091d9b6d7f5d2a7b407d4e3a26bb
Yogita Singh <115174566+YogitaSingh9@users.noreply.github.com>
2023-07-29 23:59:16 +0530

    Update palindrome.py

--- Functions/palindrome.py
+++ Functions/palindrome.py
@@ -1,6 +1,8 @@
-string = input('Enter the string: ')
-rev = ''.join(reversed(string))
-if (string == rev):
-  print(string,'is a Palindrome')
-else:
-  print(string,'is not a Palindrome')
+def palindrome(string):
+  rev = ''.join(reversed(string))
+  if (string == rev):
+    print(string,'is a Palindrome')
+  else:
+    print(string,'is not a Palindrome')
+str=input("Enter the string : ")
+palindrome(str)
