7dbdf0f86b9bfece8169ad2b80c1c26a4ae7c8f3
Yogita Singh <115174566+YogitaSingh9@users.noreply.github.com>
2023-08-04 23:35:24 +0530

    Add files via upload

---
+++ lists/basic.py
@@ -0,0 +1,8 @@
+a=[]
+n=int(input("enter the size of list :"))
+for i in range(0,n):
+    ele=int(input("enter the element: "))
+    a.append(ele) #append is a pre-defined method used to add a single item to certain collection types.
+print(a)
+
+
