0d8d0d4b6c5153c91cd65fac3e1b9c855e24fac4
Yogita Singh <115174566+YogitaSingh9@users.noreply.github.com>
2023-08-05 23:39:48 +0530

    Add files via upload

---
+++ lists/min_max.py
@@ -0,0 +1,10 @@
+list1 = [1, 3, 4, 5, 2, 6]
+list2 = [3, 4, 8, 3, 10, 1]
+print ("The original list 1 is : " + str(list1))
+print ("The original list 2 is : " + str(list2))
+
+maxele = max(list1 +list2)
+minele = min(list1 + list2)
+
+print ("The maximum of both lists is : " + str(maxele))
+print ("The minimum of both lists is : " + str(minele))
